0120c429223bea8f223e507a393bf2264dbda613
Alireza Goudarzi <alireza@goudarzi.ws>
2023-07-20 03:03:31 +0300

    Installation and configuration of react-native-svg-transformer. This will help us to transform and use .svg files as Components

--- starter-code/jest.config.js
+++ starter-code/jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    '<rootDir>/src/configs/jestConfig.ts',
+  ],
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+  },
 };
