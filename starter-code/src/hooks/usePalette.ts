e6161a41c30287c1f0aef6f239d529dc1f4742df
Alireza Goudarzi <alireza@goudarzi.ws>
2023-07-20 03:30:24 +0300

    Improved usePalette.ts to check persisted lighting settings in Redux. Now, users can manually set their preference for dark mode or light mode, or use the system settings configuration.

--- starter-code/src/hooks/usePalette.ts
+++ starter-code/src/hooks/usePalette.ts
@@ -1,11 +1,14 @@
 import {useColorScheme} from 'react-native';
 import {themeConfig} from '../configs/themeConfig';
+import {useSettings} from './useSettings';
 
 export const usePalette = () => {
   const scheme = useColorScheme();
+  const {data} = useSettings();
+  const lighting = data?.lighting;
 
   return {
-    palette: themeConfig.palette[scheme || 'dark'],
+    palette: themeConfig.palette[lighting || scheme || 'dark'],
     scheme,
   };
 };
